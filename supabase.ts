e27cfb154f3d7f026d1041ac6b47d2f490963003
naimul <naimul1005@gmail.com>
2026-03-23 22:12:02 +0000

    update supabase ts and env

--- supabase.ts
+++ supabase.ts
@@ -8,6 +8,7 @@ if (!supabaseUrl || !supabaseAnonKey) {
 }
 
 export const supabase = createClient(
+  // Add your correct supabase URL and anon key
   supabaseUrl || 'https://.supabase.co',
   supabaseAnonKey || '.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlc2JmdWlmanJjbXJjemd1c2J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxMDk0ODcsImV4cCI6MjA4OTY4NTQ4N30.2dWx9YfRZH8oWR704POwzuYTkF3MujSzsGzhkwwugLM'
 )
